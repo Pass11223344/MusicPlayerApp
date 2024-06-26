7a533d86b1f00fb729a7724b688869a75c64753c
Pass11223344 <138595065+Pass11223344@users.noreply.github.com>
2024-06-26 16:58:43 +0800

    Delete wangyiyun directory

--- wangyiyun/app/src/main/java/com/rikkatheworld/wangyiyun/service/IPlayerViewChange.java
+++
@@ -1,9 +0,0 @@
-package com.rikkatheworld.wangyiyun.service;
-//视图更新
-public interface IPlayerViewChange {
-
-
-    //进度条状态
-    void onSeekChange(int seek);
-    void RotateView();
-}
--- wangyiyun/app/src/main/java/com/rikkatheworld/wangyiyun/util/NetworkInfo.java
+++
@@ -1,5 +0,0 @@
-package com.rikkatheworld.wangyiyun.util;
-
-public class NetworkInfo {
-    public final static  String URL = "https://www.consistent.top";
-}
